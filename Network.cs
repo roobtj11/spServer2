db9c11ca3dceb827e1defd3a9315c042c1efea61
a-troob <tylerroob@gmail.com>
2022-05-02 17:43:19 -0500

    Added ability to go over 2000 buffer by seperating into multiple packets

--- Network.cs
+++ Network.cs
@@ -18,17 +18,61 @@ public static class Network
         var buffer = new byte[2048];
         var numBytesReceived = socket.Receive(buffer);
         var textReceived = Encoding.ASCII.GetString(buffer, 0, numBytesReceived);
+
+        if (textReceived.StartsWith("INCOMPLETE"))
+        {
+            string combinedtext = null;
+            while (textReceived.StartsWith("INCOMPLETE"))
+            {
+                combinedtext = combinedtext + textReceived.Substring(10);
+                textReceived = incomplete_reciever(socket);
+            }
+            textReceived = combinedtext + textReceived;
+        }
+
         string[] a = textReceived.Split("\r");
         return a[0];
+    }
+
+    public static string incomplete_reciever(Socket socket)
+    {
+        var buffer = new byte[2048];
+        var numBytesReceived = socket.Receive(buffer);
+        var textReceived = Encoding.ASCII.GetString(buffer, 0, numBytesReceived);
+        return textReceived;
+
     }
 
     public static void sendmessage(Socket socket, string message)
     {
         message = message + "\r";
         //Console.WriteLine(message.Length.ToString());
+        string WholeMessage = message;
+        
 
         var bytes = Encoding.ASCII.GetBytes(message);
-        socket.Send(bytes);
+        List<byte[]?> packets = new List<byte[]?>();
+        if(bytes.Length > 2000)
+        {
+            
+            while (bytes.Length > 2000)
+            {
+                message = "INCOMPLETE" + message;
+                string newmessage = message.Substring(0, message.Length / 2);
+                message = message.Substring(newmessage.Length);
+                bytes = Encoding.ASCII.GetBytes(newmessage);
+                packets.Add(bytes);
+            }
+            packets.Add(Encoding.ASCII.GetBytes(message));
+            foreach(var packet in packets)
+            {
+                socket.Send(packet);
+            }
+        }
+        else
+        {
+            socket.Send(bytes);
+        }
     }
 
 }
